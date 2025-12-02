2ac01b829e0577794742852f9eabe0fd6cdd4c86
Brian Gibbs <briangibbs7@syberjet.com>
2025-12-02 04:43:33 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -88,6 +88,7 @@ const navigation = [
   { name: 'Social Media', icon: Share2, page: 'SocialMedia' },
   { name: 'Social Listening', icon: Radio, page: 'SocialListening' },
   { name: 'Social Calendar', icon: CalendarDays, page: 'SocialCalendar' },
+  { name: 'Hashtag Pool', icon: Target, page: 'HashtagManager' },
   { name: 'divider', label: 'Collaboration' },
   { name: 'Team Projects', icon: Users, page: 'Collaboration' },
   { name: 'divider', label: 'Assets' },
