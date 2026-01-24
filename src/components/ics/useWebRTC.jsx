9f1d18fa2c576a6a996adf733c748a02e2a2e080
Brian Gibbs <briangibbs7@syberjet.com>
2026-01-24 22:07:41 +0000

    File changes

--- src/components/ics/useWebRTC.jsx
+++ src/components/ics/useWebRTC.jsx
@@ -202,6 +202,7 @@ export function useWebRTC(roomId, user, participants) {
     toggleVideo,
     changeVideoQuality,
     startScreenShare,
+    stopScreenShare,
     createPeerConnection,
     cleanupConnections,
   };
