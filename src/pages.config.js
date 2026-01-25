a1775de2ad84beaaa49f5c02c331bdd3d928a7ad
Brian Gibbs <briangibbs7@syberjet.com>
2026-01-25 21:48:33 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -90,6 +90,7 @@ import WebDashboard from './pages/WebDashboard';
 import WikiPageEditor from './pages/WikiPageEditor';
 import WorkflowBuilder from './pages/WorkflowBuilder';
 import WorkflowEngine from './pages/WorkflowEngine';
+import FeedbackManagement from './pages/FeedbackManagement';
 import __Layout from './Layout.jsx';
 
 
@@ -186,6 +187,7 @@ export const PAGES = {
     "WikiPageEditor": WikiPageEditor,
     "WorkflowBuilder": WorkflowBuilder,
     "WorkflowEngine": WorkflowEngine,
+    "FeedbackManagement": FeedbackManagement,
 }
 
 export const pagesConfig = {
