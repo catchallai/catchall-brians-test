e1251dca6db2899b46df43db2ed82fc4a1684ddc
Sean Hetzel <seanhetzel81@syberjet.com>
2026-03-19 11:17:13 -0700

    chore(1828): add format script and prettierignore

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import pluginJs from '@eslint/js';
 import pluginReact from 'eslint-plugin-react';
 import pluginReactHooks from 'eslint-plugin-react-hooks';
 import pluginUnusedImports from 'eslint-plugin-unused-imports';
+import pluginJson from 'eslint-plugin-json';
 
 export default [
   {
@@ -57,6 +58,15 @@ export default [
           argsIgnorePattern: '^_',
         },
       ],
+    },
+  },
+  {
+    files: ['**/*.json'],
+    plugins: {
+      json: pluginJson,
+    },
+    rules: {
+      'json/*': ['error'],
     },
   },
 ];
