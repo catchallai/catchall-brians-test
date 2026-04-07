3e395dfc5e8fb0caa2c18bf6b3b1e08ca7be5298
Chet Carter <chetcarter@me.com>
2026-04-07 10:17:19 -0700

    feat(2085): add platform constants and TikTokIcon component

---
+++ src/constants/platforms.ts
@@ -0,0 +1,58 @@
+// src/constants/platforms.ts
+import { Twitter, Linkedin, Facebook, Instagram, Youtube } from 'lucide-react';
+import { TikTokIcon } from '@/components/icons/TikTokIcon';
+
+export const PLATFORMS = [
+  {
+    id: 'Twitter',
+    label: 'X',
+    icon: Twitter,
+    bg: '#000000',
+    tailwind: 'bg-black',
+    limit: 280,
+  },
+  {
+    id: 'LinkedIn',
+    label: 'LinkedIn',
+    icon: Linkedin,
+    bg: '#0a66c2',
+    tailwind: 'bg-blue-700',
+    limit: 3000,
+  },
+  {
+    id: 'Facebook',
+    label: 'Facebook',
+    icon: Facebook,
+    bg: '#1877f2',
+    tailwind: 'bg-blue-600',
+    limit: 63206,
+  },
+  {
+    id: 'Instagram',
+    label: 'Instagram',
+    icon: Instagram,
+    bg: '#e1306c',
+    tailwind: 'bg-pink-600',
+    limit: 2200,
+  },
+  {
+    id: 'YouTube',
+    label: 'YouTube',
+    icon: Youtube,
+    bg: '#ff0000',
+    tailwind: 'bg-red-600',
+    limit: 5000,
+  },
+  {
+    id: 'TikTok',
+    label: 'TikTok',
+    icon: TikTokIcon,
+    bg: '#010101',
+    tailwind: 'bg-black',
+    limit: 2200,
+  },
+] as const;
+
+export const PLATFORM_MAP = Object.fromEntries(PLATFORMS.map((p) => [p.id, p]));
+
+export type PlatformId = (typeof PLATFORMS)[number]['id'];
